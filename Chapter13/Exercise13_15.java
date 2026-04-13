d3bab058f7ff999e598901ce0c8d49415a1597e0
Ellyn Hindle <ellyn.hindle@gmail.com>
2026-04-13 19:21:32 -0400

    second exercise

--- Chapter13/Exercise13_15.java
+++ Chapter13/Exercise13_15.java
@@ -33,37 +33,102 @@ public class Exercise13_15 {
 // Name the revised Rational class RationalUsingBigInteger 
 class RationalUsingBigInteger extends Number 
     implements Comparable<RationalUsingBigInteger> {
-  public RationalUsingBigInteger(BigInteger bigInteger, BigInteger bigInteger2) {
-    //TODO Auto-generated constructor stub
-    }
+
+
   // Data fields for numerator and denominator
   private BigInteger numerator = BigInteger.ZERO;
   private BigInteger denominator = BigInteger.ONE;
+
+  public RationalUsingBigInteger(){
+    this.numerator = BigInteger.ZERO;
+    this.denominator = BigInteger.ONE;
+  }
   
+  public RationalUsingBigInteger(BigInteger bigInteger, BigInteger bigInteger2) {
+    BigInteger gcd = gcd(numerator, denominator);
+    this.numerator = ((denominator > 0) ? 1 : -1) * numerator / gcd;
+    this.denominator = Math.abs(getDenominator()) / gcd;   
+    }
+
+/**     private static BigInteger gcd(BigInteger n, BigInteger d){
+      int n1 = abs(n);
+      int n2 = abs(d);
+      int gcd = 1;
+
+    for (int k = 1; k <= n1 && k <= n2; k++) {
+      if (n1 % k == 0 && n2 % k == 0) 
+        gcd = k;
+    }
+
+    return gcd;
+    }*/
+
+    /** Return numerator */
+  public BigInteger getNumerator() {
+    return numerator;
+  }
+
+  /** Return denominator */
+  public BigInteger getDenominator() {
+    return denominator;
+  }
+
+  /** Add a rational number to this rational */
+  public RationalUsingBigInteger add(RationalUsingBigInteger secondRational) {
+    BigInteger n = (numerator.multiply(secondRational.getDenominator())).add(denominator.multiply(secondRational.getNumerator()));
+    BigInteger d = denominator.multiply(secondRational.getDenominator());
+    return new RationalUsingBigInteger(n, d);
+  }
+
+  /** Subtract a rational number from this rational */
+  public RationalUsingBigInteger subtract(RationalUsingBigInteger secondRational) {
+    BigInteger n = numerator.multiply(secondRational.getDenominator()).subtract(denominator.multiply(secondRational.getNumerator()));
+    BigInteger d = denominator.multiply(secondRational.getDenominator());
+    return new RationalUsingBigInteger(n, d);
+  }
+
+  /** Multiply a rational number to this rational */
+  public RationalUsingBigInteger multiply(RationalUsingBigInteger secondRational) {
+    BigInteger n = numerator.multiply(secondRational.getNumerator());
+    BigInteger d = denominator.multiply(secondRational.getDenominator());
+    return new RationalUsingBigInteger(n, d);
+  }
+
+  /** Divide a rational number from this rational */
+  public RationalUsingBigInteger divide(RationalUsingBigInteger secondRational) {
+    BigInteger n = numerator.multiply(secondRational.getDenominator());
+    BigInteger d = denominator.multiply(secondRational.getNumerator());
+    return new RationalUsingBigInteger(n, d);
+  }
+
   @Override
   public int compareTo(RationalUsingBigInteger o) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'compareTo'");
+    if (this.subtract(o).getNumerator() > 0)
+      return 1;
+    else if (this.subtract(o).getNumerator() < 0)
+      return -1;
+    else 
+      return 0;
   }
   @Override
   public int intValue() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'intValue'");
+    return (int)doubleValue();
   }
   @Override
   public long longValue() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'longValue'");
+    return (long)doubleValue();
   }
   @Override
   public float floatValue() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'floatValue'");
+    return (float)doubleValue();
   }
-  @Override
+
+   @Override
   public double doubleValue() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'doubleValue'");
+    double dNumerator = this.getNumerator();
+
+    //*figure out how to convert this 
+    return (numerator.multiply((long) 1.0)).divide(this.getDenominator());
   }
 
   // WRITE YOUR CODE
